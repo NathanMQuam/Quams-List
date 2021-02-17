790c06effa4f0d09d70a4a8433dce562a92f33ec
Nathan Quam <Nathan.M.Quam@gmail.com>
2021-02-17 15:01:16 -0700

    Houses

--- app/main.js
+++ app/main.js
@@ -1,10 +1,12 @@
 import CarsController from "./Controllers/CarsController.js";
-import ValuesController from "./Controllers/ValuesController.js";
+import housesController from "./Controllers/HousesController.js";
+// import ValuesController from "./Controllers/ValuesController.js";
 
 
 class App {
   // valuesController = new ValuesController();
   carsController = new CarsController();
+  housesController = new housesController();
 }
 
 window["app"] = new App();
